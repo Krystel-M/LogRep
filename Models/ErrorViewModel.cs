e5374779464096fa9aabfb04a90106768b909890
Krystel Morales <krystelmorales72@outlook.com>
2023-04-30 20:14:34 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogRep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
