815cb520bc194ad42455bb2a56748eba5dd75212
Krystel Morales <krystelmorales72@outlook.com>
2023-05-01 10:53:00 -0500

    ITS ALIVE

--- Program.cs
+++ Program.cs
@@ -4,15 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddControllersWithViews();
+
+
+var connectionString = "server=localhost;user=log;password=log;database=log";
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 33));
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+  //  options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<RecipeContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<RecipeContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
 
 var app = builder.Build();
 
@@ -27,6 +32,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Add services to the container.
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
